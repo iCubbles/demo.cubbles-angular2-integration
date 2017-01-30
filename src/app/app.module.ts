66cc3c4bf85b81ae5a0d83e09de6b9e6ef46fd23
Steffen Sommer <Steffen Sommer>
2017-01-30 06:54:13 +0100

    created the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { Demo1Component } from './demo1/demo1.component';
 import { Demo2Component } from './demo2/demo2.component';
 import { Demo3Component } from './demo3/demo3.component';
 import { PieChartDirective } from './pie-chart.directive';
+
+const appRoutes: Routes = [
+  { path: 'demo1', component: Demo1Component },
+  { path: 'demo2', component: Demo2Component },
+  { path: 'demo3', component: Demo3Component },
+  {
+    path: '',
+    redirectTo: '/demo1',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +32,8 @@ import { PieChartDirective } from './pie-chart.directive';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
