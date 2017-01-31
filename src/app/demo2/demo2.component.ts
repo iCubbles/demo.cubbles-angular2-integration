b42ba3461d4e76e7532065a411c02ddecf56fbaa
Steffen Sommer <Steffen Sommer>
2017-01-31 08:44:38 +0100

    added explanation

--- src/app/demo2/demo2.component.ts
+++ src/app/demo2/demo2.component.ts
@@ -17,6 +17,7 @@ export class Demo2Component implements OnInit {
     // build custom event for starting bootstrap of CIF (here, use the deprecated way that also works in IE)
     var event = document.createEvent('CustomEvent');
     event.initCustomEvent('CubxComponentLinked', true, true, {});
+    
     // dispatch this 'CubxComponentLinked' event
     document.dispatchEvent(event);
   }
