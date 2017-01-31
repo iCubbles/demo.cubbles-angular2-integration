43763328869d739de4e93342775bf3c7ec793c63
Steffen Sommer <Steffen Sommer>
2017-01-31 09:13:19 +0100

    added a simple  demo3

--- src/app/demo3/demo3.component.ts
+++ src/app/demo3/demo3.component.ts
@@ -9,6 +9,12 @@ export class Demo3Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // build custom event for starting bootstrap of CIF (here, use the deprecated way that also works in IE)
+    var event = document.createEvent('CustomEvent');
+    event.initCustomEvent('CubxComponentLinked', true, true, {});
+
+    // dispatch this 'CubxComponentLinked' event
+    document.dispatchEvent(event);
   }
 
 }
