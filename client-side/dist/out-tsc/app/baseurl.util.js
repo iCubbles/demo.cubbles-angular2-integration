c4629e80dedc2df967176be870454a445a2a1fc7
Judit Ross <jtrs@incowia.com>
2017-03-07 16:15:33 +0100

    RTE.67: move dependencies in CRCInit

---
+++ client-side/dist/out-tsc/app/baseurl.util.js
@@ -0,0 +1,5 @@
+export function getBaseUrl() {
+    var baseUrl = window.location.pathname.replace(/\/demo\d/, '');
+    return baseUrl;
+}
+//# sourceMappingURL=D:/Projekte/Cubbles/github/icubbles/demo.cubbles-angular2-integration/client-side/src/app/baseurl.util.js.map
